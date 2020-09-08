3f067652ef6046a37748deb0bfc1ee0018923bfc
Spandan Pal <2spandan2@gmail.com>
2020-09-09 00:51:23 +0530

    trial run 3

--- sendAlert.py
+++ sendAlert.py
@@ -1,6 +1,8 @@
 import os, time, requests
 from boltiot import Sms, Email
 
+# later use time to avoid alerts at times you are busy
+
 def send_simple_message():
 	return requests.post(
 		"https://api.mailgun.net/v3/sandboxcc538641e93641148fe24b216f0e0fec.mailgun.org/messages",
@@ -10,16 +12,14 @@ def send_simple_message():
 			"subject": "Hello",
 			"text": "Testing some Mailgun awesomness!"})
 
-# print(os.environ['HOME'])
+if(os.environ['IS_ACTIVATED']==1):
+    response=send_simple_message()
+    print("Response EMAIL",response)
 
-response=send_simple_message()
-print("Response EMAIL",response)
-sms = Sms(os.environ['SSID'], os.environ['AUTH_TOKEN'], os.environ['TO_NUMBER'], os.environ['FROM_NUMBER'])
-# mailer = Email(os.environ['MAILGUN_API_KEY'], os.environ['SANDBOX_URL'], os.environ['SENDER_EMAIL'], os.environ['RECIPIENT_EMAIL'])
+    sms = Sms(os.environ['SSID'], os.environ['AUTH_TOKEN'], os.environ['TO_NUMBER'], os.environ['FROM_NUMBER'])
+    # mailer = Email(os.environ['MAILGUN_API_KEY'], os.environ['SANDBOX_URL'], os.environ['SENDER_EMAIL'], os.environ['RECIPIENT_EMAIL'])
 
-# later use time to avoid alerts at times you are busy
-
-response = sms.send_sms("Testing")
-print("Response SMS",response)
-# response = mailer.send_email("Alert", "Testing" )
-# print("Response EMAIL",response)
+    response = sms.send_sms("Testing")
+    print("Response SMS",response)
+    # response = mailer.send_email("Alert", "Testing" )
+    # print("Response EMAIL",response)
